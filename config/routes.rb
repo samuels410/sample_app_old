5872f44402f0c530aa8b100e51a25451a000efcb
samuelrails <samuels410@gmail.com>
2013-02-21 11:28:21 +0530

    mc deledte compleres

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+SampleApp::Application.routes.draw do
+	resources :users
+	resources :sessions, only: [:new, :create, :destroy]
+	resources :microposts, only: [:create, :destroy]
+
+	root to: 'static_pages#home'
+
+	match '/signup' ,    to: 'Users#new'
+	match '/show' ,      to: 'Users#show'
+	match '/help' ,      to: 'static_pages#help'
+	match '/about' ,     to: 'static_pages#about'
+	match '/contact' ,   to: 'static_pages#contact'
+	match '/signin',     to: 'sessions#new'
+  match '/sessions',   to: 'sessions#create'
+	match '/signout',    to: 'sessions#destroy', via: :delete
+end
